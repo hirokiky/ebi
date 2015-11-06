d78c859a4eb769c18e76892f7d4045cf305d5df3
Hiroki KIYOHARA <hirokiky@gmail.com>
2015-11-06 18:57:09 +0900

    Fixed to removed created version zip file

--- ebi/appversion.py
+++ ebi/appversion.py
@@ -58,15 +58,18 @@ def upload_app_version(app_name, bundled_zip):
 
 def make_application_version(app_name, version, dockerrun, ebext):
     bundled_zip = make_version_file(version, dockerrun=dockerrun, ebext=ebext)
-    bucket, key = upload_app_version(app_name, bundled_zip)
+    try:
+        bucket, key = upload_app_version(app_name, bundled_zip)
 
-    logger.info('Creating application version')
-    eb = boto3.client('elasticbeanstalk')
-    eb.create_application_version(
-        ApplicationName=app_name,
-        VersionLabel=version,
-        SourceBundle={
-            'S3Bucket': bucket,
-            'S3Key': key,
-        }
-    )
+        logger.info('Creating application version')
+        eb = boto3.client('elasticbeanstalk')
+        eb.create_application_version(
+            ApplicationName=app_name,
+            VersionLabel=version,
+            SourceBundle={
+                'S3Bucket': bucket,
+                'S3Key': key,
+            }
+        )
+    finally:
+        os.remove(bundled_zip)
