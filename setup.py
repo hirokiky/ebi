ca3f5ec590fe6f853c45e920fe9987d14f5e3aeb
Hiroki KIYOHARA <hirokiky@gmail.com>
2016-03-07 11:01:28 +0900

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('./README.rst').read()
 
 setup(
     name='ebi',
-    version='0.5.1',
+    version='0.6',
     install_requires=[
         'awsebcli==3.7.3',
         'boto3==1.2.6',
