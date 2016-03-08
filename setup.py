3ec8533ffdcbc15e2cd2e5f55c4038ea45e15d0f
osamunmun <osamu.takayasu@nex.nikkei.com>
2016-03-08 11:24:56 +0900

    Bumped version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('./README.rst').read()
 
 setup(
     name='ebi',
-    version='0.6',
+    version='0.6.2',
     install_requires=[
         'awsebcli==3.7.3',
         'boto3==1.2.6',
