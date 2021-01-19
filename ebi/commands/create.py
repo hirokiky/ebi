d24fa0207e9c7f9329e0349b76913b32ef9a147c
ray1005yb <ray1005rb0@gmail.com>
2021-01-19 19:00:35 +0900

    add ebignore params

--- ebi/commands/create.py
+++ ebi/commands/create.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 def main(parsed):
     if parsed.version:
         version = parsed.version
-    elif parsed.prefix :
+    elif parsed.prefix:
         version = "{}_{}".format(parsed.prefix, int(time.time()))
     else:
         version = str(int(time.time()))
@@ -21,7 +21,7 @@ def main(parsed):
     else:
         description = ''
 
-    appversion.make_application_version(parsed.app_name, version, parsed.dockerrun, parsed.docker_compose, parsed.ebext, description)
+    appversion.make_application_version(parsed.app_name, version, parsed.dockerrun, parsed.docker_compose, parsed.ebext, parsed.ebignore, description)
 
     logger.info('Ok, now creating version %s for environment %s', version, parsed.env_name)
     payload = ['eb', 'create', parsed.env_name,
@@ -49,5 +49,7 @@ def apply_args(parser):
     parser.add_argument('--dockerrun', help='Path to file used as Dockerrun.aws.json')
     parser.add_argument('--docker-compose', help='Path to file used as docker-compose.yml')
     parser.add_argument('--ebext', help='Path to directory used as .ebextensions/')
+    parser.add_argument('--ebignore', help='Zip project based on .ebignore',
+                        action='store_true', default=False)
     parser.add_argument('--cfg', help='Configuration template name to eb create')
     parser.set_defaults(func=main)
